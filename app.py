6572f708dcc814a32a063c73cb8f758200b98ea5
Raul Andrial <rauljandrial@gmail.com>
2024-08-22 04:44:20 -0400

    created streamlit frontend ui

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+import streamlit as st
+
+st.set_page_config(page_title='Chat with IRS Pub17 2023', page_icon=':books:')
+
 def main():
-    print('hello world')
+    
+    st.header('Chat with IRS Pub17 2023 :books:')
+    st.text_input('Ask a tax question:')
+
+with st.sidebar:
+    st.subheader('pdf')
+    st.file_uploader('upload here and click on process')
+    st.button('process')
 
 if __name__ == '__main__':
     main()
