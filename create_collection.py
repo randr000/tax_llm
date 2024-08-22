9d2eba1e8c483d6d751bcb5fd1db83872080f5d8
Raul Andrial <rauljandrial@gmail.com>
2024-08-22 14:38:49 -0400

    create qdrant pub17 collection

---
+++ create_collection.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain_community.vectorstores import Qdrant
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+import qdrant_client
+import os
+
+import qdrant_client.models
+
+load_dotenv()
+QDRANT_API_KEY = os.getenv('QDRANT_API_KEY')
+QDRANT_HOST = f"{os.getenv('QDRANT_HOST')}/"
+QDRANT_COLLECTION_NAME = os.getenv('QDRANT_COLLECTION_NAME')
+
+# create a qdrant client
+
+client = qdrant_client.QdrantClient(
+    url=QDRANT_HOST,
+    api_key=QDRANT_API_KEY
+)
+
+# create collection
+client.create_collection(
+    collection_name=QDRANT_COLLECTION_NAME,
+    vectors_config=qdrant_client.models.VectorParams(size=768, distance=qdrant_client.models.Distance.COSINE)
+)
