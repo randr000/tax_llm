7ff149e49f690629fb63c680af6301d8c1070999
Raul Andrial <rauljandrial@gmail.com>
2024-08-23 15:35:18 -0400

    local query response working with fastapi

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from chat import retrieve, chat
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/query')
-async def root():
-    return {'message': 'hello world'}
+qa = retrieve()
+
+class Query(BaseModel):
+    query: str
+    response: str | None = None
+
+@app.post('/query')
+async def chat_with_doc(query: Query) -> Query:
+    return {
+        'query': query.query,
+        'response': chat(qa, query.query)
+    }
