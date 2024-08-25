b10a72ba75195072756e72e1edcc3874ff1aa15f
Raul Andrial <rauljandrial@gmail.com>
2024-08-25 08:16:48 -0400

    allowed CORS and updated gitignore it ignore pycache folder

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from chat import retrieve, chat
 from pydantic import BaseModel
 
 app = FastAPI()
+
+origins = [
+    'http://localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=['POST'],
+    allow_headers=['*']
+)
 
 qa = retrieve()
 
